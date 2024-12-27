eb15edb36ea56208619957b6ea74c4bde0a122ec
jahmi-chaima <jahmichaima333@gmail.com>
2024-12-27 17:50:08 +0100

    Ajout des fichiers du projet initial

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ContactForm from './ContactForm'; // Assurez-vous que le chemin est correct
+
+function App() {
+  return (
+    <div className="App">
+     
+      <ContactForm />
+    </div>
+  );
+}
+
+export default App;
+
