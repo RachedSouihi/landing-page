52acd3d1e254be29ce13911c8e15b01f5e0c0728
RachedSouihi <Souihiraached@gmail.com>
2024-12-27 19:05:26 +0100

    Added contact form

--- src/components/Courses/CourseCard.js
+++ src/components/Courses/CourseCard.js
@@ -1,15 +1,27 @@
-import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
+import * as React from "react";
+import Card from "@mui/material/Card";
 
-export default function CourseCard() {
+import "./courses.css"
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+
+export default function CourseCard(props) {
   return (
-    <div>
-
+    
+    <div class="card course-card">
+      <img src={props.image} class="card-img-top"  />
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">
+          {props.title}
+        </p>
+        <a href="#" class="btn btn-primary">
+          Go somewhere
+        </a>
+      </div>
     </div>
   );
 }
--- src/components/Courses/Courses.js
+++ src/components/Courses/Courses.js
@@ -1,13 +1,56 @@
 import React, { useState, useEffect } from 'react';
-import CourseCard from './CourseCard.js';
+import CourseCard from './CourseCard';
+import "./courses.css"
+import { Container } from '@mui/material';
 
 export default function Courses() {
     const [courses, setCourses] = useState([]);
 
 
+    useEffect(() => {
+        setCourses( [
+            {
+              "id": 1,
+              "title": "Spring Boot / Angular",
+              "price": "350 DT/Month",
+              "image": "/assets/images/node-react.png"
+            },
+            {
+              "id": 2,
+              "title": "Node JS / React",
+              "price": "350 DT/Month",
+              "image": "/assets/images/node-react.png"
+            },{
+                "id": 3,
+                "title": "Spring Boot / Angular",
+                "price": "350 DT/Month",
+                "image": "/assets/images/node-react.png"
+              },
+              {
+                "id": 4,
+                "title": "Node JS / React",
+                "price": "350 DT/Month",
+                "image": "/assets/images/node-react.png"
+              }
+          ])
+    }, [])
+
+
+
+
+
   return (
-    <div className="courses-grid">
-      
+    <div className="container-md-10">
+
+        <div className='row mx-auto justify-content-center'>
+            {courses.map((course) => (
+                <div className='col-4'>
+                    <CourseCard key={course.id} {...course} />
+                </div>
+            ))}
+
+      </div>
+
     </div>
   );
 };
